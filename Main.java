07aee235e90d038151a55e4d0f8ddc8927e4324b
Ava Tran <ava.tran@patelhs.org>
2021-02-22 14:57:49 +0000

    putting in scanner so we can scan for user's imput

--- Main.java
+++ Main.java
@@ -1,27 +1,29 @@
-import java.util.Scanner;
+import java.util.Scanner; 
+
 class Main {
-  public static void main(String[] args)
-   {
-    Scanner scan = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in); //used for user's imput
 
     System.out.println("Hi! Welcome to Ava's Ice Cream Shop. How many scoops of ice cream would you like?");
     int scoops = scan.nextInt();
-    int totalScoops = iceCream(scoops); 
-
-    int count = 0;  
-    int total = finalAmount;  
+    int count = 1; //will remain 1 as we can't do 0
+    iceCream(scoops, count); //calling the method 
   }
-
-  static int iceCream(int scoops){
-
-    if(count <= scoops){
-
-    System.out.println("What is the cost of the flavor that you want? ");
-    String cost = scan.next();
-
-    total = total + cost;
+  
+  static boolean iceCream(int scoops, int count) {
+    Scanner scan = new Scanner(System.in);
+    if (count <= scoops) { 
+      //seeing how many scoops you have considering what you put into imput.
+      //all scoops have to be less or equal to 1, or else you already have all your scoops. 
+      System.out.println("you're at " + count + " scoop.");
+      System.out.println("What flavor would you like? "); //asking what flavor user wants
+      String flavor = scan.next();
+      count++; //count adds 1 after every time
+      return iceCream(scoops, count);
+    } else { //else statement if if statement is not fulfulled
+      System.out.println("You're ice cream is ready! ");
+      return false;
     }
-    
 
   }
 }
